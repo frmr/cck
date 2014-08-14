341b049392170974fb9630becfccfe0566284703
Farmer <farmergeddon@gmail.com>
2014-08-14 12:57:04 +0100

    Added cckData.h and cckData.cpp.

---
+++ src/cckData.cpp
@@ -0,0 +1,7 @@
+#include "cckData.h"
+
+cck::Data::Data( const int id, const double height )
+	:	id( id ),
+		height( height )
+{
+}
---
+++ src/cckData.h
@@ -0,0 +1,17 @@
+#ifndef CCK_DATA_H
+#define CCK_DATA_H
+
+namespace cck
+{
+	class Data
+	{
+		public:
+			int		id;
+			double	height;
+
+		public:
+			Data( const int id, const double height );
+	};
+}
+
+#endif // CCK_DATA_H
